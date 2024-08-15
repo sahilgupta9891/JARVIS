21fe1908d37f9cbe6b2f4a4f1a44e35562efc516
sahilgupta9891 <sahilgupta271204@gmail.com>
2024-08-15 14:55:17 +0530

    feat: added code files

--- background.js
+++ background.js
@@ -1,22 +1,22 @@
-console.log( " Starting background.js")
+console.log("Starting background.js")
 const apikey  = "";
-const url= "https://api.openai.com/v1/chat/completions ";
+const url= "https://api.openai.com/v1/chat/completions";
 async function query(data) {
-    const respone = await fetch (
+    const response = await fetch (
         "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2",
         {
-            headers : { Authorization : " Bearer"},
+            headers : { Authorization : "Bearer"},
             method : "POST",
             body: JSON.stringify(data),
         }
     );
-    const result = await respone.json();
+    const result = await response.json();
     return result;
 }
 async function OpenaiFetchAPI(messages) {
     console.log("Using OPENAI function");
     console.log(messages);
-    console.log("Calling GPT3");
+    console.log("Calling GPT4");
     const data = {
         model: "gpt-4o-mini",
         messages:messages,
@@ -30,7 +30,7 @@ async function OpenaiFetchAPI(messages) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${apiKey}`
+           "Authorization": `Bearer ${apiKey}`
         },
         body: JSON.stringify(data),
       };
@@ -49,7 +49,7 @@ async function OpenaiFetchAPI(messages) {
 
 }
 class Jarvis{
-    constructor( command){
+    constructor(command){
         this.command= command;
         //openai credentials
     }
@@ -62,7 +62,7 @@ class Jarvis{
                 "content ": "You have only one job . You will be given statement about navigating through tabs  in two directions. Forward or backward .You have to ONLY return  the numerical value of the number of times to go forward or backward .Do not return ANYTHING else "
                 },
                 {    
-                    "role": user,
+                    "role": "user",
                     "content": this.command
                 },
             ]
@@ -94,8 +94,8 @@ class Jarvis{
         const urls=[];
         for( i=0; i<x; i++){
             const site = names[i];
-            let here = ( " https://*."+site+".com/*");
-            const tabs = await chrome.tabs.query({ url: String(here)});
+            let here = ("https://*."+site+".com/*");
+            const tabs = await chrome.tabs.query({url: String(here)});
             let j;
             for( j=0; j< tabs.length; j++){
                 IDs.push(tabs[j].id);
@@ -117,18 +117,18 @@ class Jarvis{
     }
     async go_forward(){
         // go forward for n number of times
-        const times = await this. parser(" number", this.command);
+        const times = await this.parser("number", this.command);
         let i;
         //run loop for n number of times
         for( i=0; i<times; i++){
-            const tabs = await chrome.tabs.query({ active: true});
+            const tabs = await chrome.tabs.query({active: true});
             console.log(tabs[0]);
             Promise.resolve(chrome.tabs.goForward(tabs[0].id));
         }
     }
     async close_current_tab(){
         // close the current working tab 
-        const tabs = await chrome.tabs.query({ active : true});
+        const tabs = await chrome.tabs.query({active : true});
         Promise.resolve(chrome.tabs.remove(tabs[0].id));
     }
     async close_multiple_tabs(IDs){
@@ -143,10 +143,10 @@ class Jarvis{
         // show website when i was bored 
         const urls = ["https://www.patatap.com/", "https://libraryofbabel.info/search.html" ,"https://www.iwastesomuchtime.com/" , "https://codepen.io/akm2/full/AGgarW", "https://stars.chromeexperiments.com/","https://orb.farm/","https://findtheinvisiblecow.com/", "https://papertoilet.com/"];
         let x = urls.length;
-        Promise.resolve(chrome.tabs.create({ url : urls[Math.floor(x*(Math.random()))]}));
+        Promise.resolve(chrome.tabs.create({url : urls[Math.floor(x*(Math.random()))]}));
     }
     async open_new(){
-        const list = await this.parser(" websites");
+        const list = await this.parser("websites");
         let x = list.length;
         let i=0;
         for(i=0; i<x; i++){
@@ -170,7 +170,7 @@ class Jarvis{
 
     }
     async reopen_closed_tabs(){
-        const items= await chrome.history.search({ text:""});
+        const items= await chrome.history.search({text:""});
         console.log(items);
         this.get_titles(items);
         console.log(items[0].hasOwnProperty("title"))
@@ -189,7 +189,7 @@ class Jarvis{
         let x = items.length;
         for(i=0;i<x; i++){
             if( index[i]>= 0.25){
-                Promise.resolve(chrome.tabs.create({ url: items[i].url}))
+                Promise.resolve(chrome.tabs.create({url: items[i].url}))
             }
         }
     }
@@ -203,7 +203,7 @@ class Jarvis{
         let answer=0;
         const messages=
         [ {
-            "role": system,
+            "role": "system",
             "content": " You are a task classifier. given a particular task to do , you just return the numerical value of the ID of the task . Here are all the task types , with their IDs as the serial numbers at the starting of their description.\n0\nA task that indicates that the user wants to open pr initiate something new ,like creating a tab op opening a tab , \n1\nA task that indicates to close the current tab session or the present session .\n2\nA task that indicate to close more than one (multiple) things  or close or shutdown tabs of a particular website .\n3\nA task that indicate that the user wants to navigate to the previous tab or go backwards. \n4\nA task that indicate that the user wants to navigate to the next tab , or go forward. \n5\nA task that the user wants to group or assimilate particular types of tabs together . \n6\nwhen the user is sad , hungry or bored. Also when you think that the command doesn't follow under any of the above categories.\n7\nwhen the user is trying to reopen closed tabs . Reopen, not open new tabs , but open already closed tabs. \n\n Remember , Only return the numerical value of the typeof task and NOTHING else.\ " 
         },
         {
@@ -252,11 +252,11 @@ class Jarvis{
     }
  }
  chrome.runtime.onMessage.addListener( async ( obj, sender, response)=>{
-    const { type ,command}= obj;
-    if( type == "NEW_COMMAND"){
+    const {type ,command}= obj;
+    if(type == "NEW_COMMAND"){
         const handler = new control (String(command));
         handler.classifier();
         console.log(command);
-        console.log( "We are at service _worker, the message has been listened");
+        console.log("We are at service _worker, the message has been listened");
     }
  });
--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const browse = document.getElementById("button")
- 
-    browse.addEventListener('click', async ()=>
-    {
-       console.log("Event listened");
-       const command = document.getElementById("command");
-       let instruction = command.value;
-       console.log("message is being sent");
-       chrome.runtime.sendMessage(
-          {type : "NEW_COMMAND", command : String(instruction)}
-        )
-    }
-    )
- })
- 
- 
- 
- 
- 
+   const browse = document.getElementById("button")
+
+   browse.addEventListener('click', async ()=>
+   {
+      console.log("Event listened");
+      const command = document.getElementById("command");
+      let instruction = command.value;
+      console.log("message is being sent");
+      chrome.runtime.sendMessage(
+         {type : "NEW_COMMAND", command : String(instruction)}
+       )
+   }
+   )
+})
